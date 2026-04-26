78704fdd5efe481a9db83b75bae20328d71b05a3
Oussama Mos <oussamabader@gmail.com>
2026-04-26 18:41:12 +0100

    [Update] Periodic update major versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     base: "/",
-    plugins: [react(), svgr()],
+    plugins: [react({}), svgr()],
     test: {
         globals: true,
         environment: "jsdom",
